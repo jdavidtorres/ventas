0c7d76d505e62c4677f3026275151ab23967e40f
Yisus <jesu_ti91@hotmail.com>
2022-10-27 10:44:57 -0500

    Creando proyecto nuevamente

---
+++ ventas/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ventas;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ventas/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ventas;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
