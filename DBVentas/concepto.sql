4b8055d64e7e94b3f33bd7e5257050a0ae2db78c
Yisus <jesu_ti91@hotmail.com>
2022-10-27 14:54:57 -0500

    Diseño DB

---
+++ DBVentas/concepto.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[concepto]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [id_venta] BIGINT NULL, 
+    [cantidad] INT NULL, 
+    [precioUnitario] DECIMAL(16, 2) NULL, 
+    [importe] DECIMAL(16, 2) NULL, 
+    [id_producto] BIGINT NULL
+)
---
+++ DBVentas/venta.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[venta]
+(
+	[id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [fecha] DATETIME NULL, 
+    [total] DECIMAL(16, 2) NULL
+)
